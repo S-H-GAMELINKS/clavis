102f7ff3e58e44b9b337dc47f7ba21ca669458f0
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-20 15:01:09 +0900

    add set_route method & using routes #19

--- ext/clavis/clavis.cpp
+++ ext/clavis/clavis.cpp
@@ -2,15 +2,18 @@
 #include <rice/Constructor.hpp>
 #include <httplib.h>
 #include <string>
+#include <unordered_map>
 
 using namespace Rice;
 
 class Clavis {
+        std::unordered_map<std::string, std::string> routes;
         std::string port;
     public:
         Clavis();
         void clavis_set_port(const std::string& port);
         const std::string clavis_return_port();
+        bool clavis_set_route(const std::string& route, const std::string& response);
         void clavis_run();
 };
 
@@ -24,19 +27,24 @@ const std::string Clavis::clavis_return_port() {
     return this->port;
 }
 
+bool Clavis::clavis_set_route(const std::string& route, const std::string& response) {
+    this->routes.emplace(route, response);
+
+    if (this->routes[route] == response)
+        return true;
+    else
+        return false;
+}
+
 void Clavis::clavis_run() {
     using namespace httplib;
 
     Server svr;
 
-    svr.Get("/", [](const Request& req, Response& res) {
-        res.set_content("Hello World!", "text/plain");
-    });
-
-    svr.Get(R"(/numbers/(\d+))", [&](const Request& req, Response& res) {
-        auto numbers = req.matches[1];
-        res.set_content(numbers, "text/plain");
-    });
+    for (auto&& r : this->routes)
+        svr.Get(r.first.c_str(), [&](const Request& req, Response& res) {
+            res.set_content(r.second.c_str(), "text/plain");
+        });
 
     svr.listen("localhost", std::stoi(this->port));
 }
@@ -47,6 +55,7 @@ extern "C" {
             .define_constructor(Constructor<Clavis>())
             .define_method("set_port", &Clavis::clavis_set_port, Rice::Arg("port"))
             .define_method("port", &Clavis::clavis_return_port)
+            .define_method("set_route", &Clavis::clavis_set_route, (Rice::Arg("route"), Rice::Arg("response")))
             .define_method("run", &Clavis::clavis_run);
     }
 }
