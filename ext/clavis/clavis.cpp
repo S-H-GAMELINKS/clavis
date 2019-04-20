51afe64f5f0852933195acf09d594c5ca8ab43eb
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-20 12:46:03 +0900

    can use running web app #13

--- ext/clavis/clavis.cpp
+++ ext/clavis/clavis.cpp
@@ -1,18 +1,38 @@
 #include <rice/Data_Type.hpp>
 #include <rice/Constructor.hpp>
+#include <httplib.h>
 
 using namespace Rice;
 
 class Clavis {
     public:
         Clavis();
+        void clavis_run();
 };
 
 Clavis::Clavis() {}
 
+void Clavis::clavis_run() {
+    using namespace httplib;
+
+    Server svr;
+
+    svr.Get("/hi", [](const Request& req, Response& res) {
+        res.set_content("Hello World!", "text/plain");
+    });
+
+    svr.Get(R"(/numbers/(\d+))", [&](const Request& req, Response& res) {
+        auto numbers = req.matches[1];
+        res.set_content(numbers, "text/plain");
+    });
+
+    svr.listen("localhost", 1234);
+}
+
 extern "C" {
     void Init_clavis() {
         Data_Type<Clavis> rb_cClavis = define_class<Clavis>("Clavis")
-            .define_constructor(Constructor<Clavis>());
+            .define_constructor(Constructor<Clavis>())
+            .define_method("run", &Clavis::clavis_run);
     }
 }
